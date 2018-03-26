d5a435df98060f4f7c02a8d9cb06cca565efbaf5
Will Chen <32256929+jacchwill@users.noreply.github.com>
2018-03-26 22:03:07 +0800

    Add files via upload

---
+++ 0326.py
@@ -0,0 +1,15 @@
+print "will"
+import time
+for yy in range(0,5):
+    print "will"
+
+def n(y):
+    for b in range(0,y):
+        print "will"
+        time.sleep(1)
+
+
+print "n=3"
+n(3)
+print "n=2"
+n(2)
