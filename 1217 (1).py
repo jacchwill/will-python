90f536b74aa4c941c6aba496de7d647df19c65b3
Will Chen <32256929+jacchwill@users.noreply.github.com>
2018-12-17 22:20:08 +0800

    Add files via upload

---
+++ 1217 (1).py
@@ -0,0 +1,22 @@
+
+# coding: utf-8
+
+# In[17]:
+
+import time
+mane="will"
+age=10
+print mane
+print age
+e=str(age)
+print mane+e
+for s in range(0,10):
+    print mane+str(s)+d
+
+d="歲"
+
+
+# In[ ]:
+
+
+
